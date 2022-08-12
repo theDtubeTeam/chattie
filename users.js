dad746199165617e550bf1e76933ef6d2a1efd9a
dakotath <toyotaparry@gmail.com>
2022-08-12 15:37:55 -0300

    Update users.js

--- users.js
+++ users.js
@@ -1,32 +1,18 @@
-const genRanHex = size => [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
-var chat = {
-  // (A) INIT CHAT
-  name : null, // USER'S NAME
-  socket : null, // CHAT WEBSOCKET
-  ewrap : null, // HTML CHAT HISTORY
-  emsg : null, // HTML CHAT MESSAGE
-  ego : null, // HTML CHAT GO BUTTON
-  init : () => {
-    // (A1) GET HTML ELEMENTS
-    chat.ewrap = document.getElementById("chatShow");
-    chat.emsg = document.getElementById("chatMsg");
-    chat.ego = document.getElementById("chatGo");
-    chat.color = genRanHex(6);
-    // (A2) USER'S NAME
-    chat.name = prompt("What is your name?", "John");
-    if (chat.name == null || chat.name=="") { chat.name = "Mysterious"; }
+let socket = new WebSocket("wss://repeated-hill-amazonsaurus.glitch.me");
+// (A5) ON RECEIVE MESSAGE - DRAW IN HTML
+chat.socket.addEventListener("getUsers", (evt) => {
+  userAdd(evt.data);
+});
+  // (D) DRAW MESSAGE IN HTML
+  userAdd(msg) {
+    // (D1) PARSE JSON
+    msg = JSON.parse(msg);
+    console.log(msg);
 
-    // (A3) CONNECT TO CHAT SERVER
-    chat.socket = new WebSocket("ws://repeated-hill-amazonsaurus.glitch.me:80");
+    // (D2) CREATE NEW ROW
+    let row = document.createElement("div");
+    row.className = "chatRow";
+    row.innerHTML = `<div class="chatName" style="background-color: #${msg["color"]};">${msg["name"]}</div>`;
+    chat.ewrap.appendChild(row);
 
-    // (A4) ON CONNECT - ANNOUNCE "I AM HERE" TO THE WORLD
-    chat.socket.addEventListener("open", () => {
-      chat.controls(1);
-      chat.send("Joined the chat room.");
-    });
-
-    // (A5) ON RECEIVE MESSAGE - DRAW IN HTML
-    chat.socket.addEventListener("user", (evt) => {
-      chat.userAdd(evt.data);
-    });
-};
+  }
